d206751994f450fa18cad500bc2c53ccd75bc6ac
John Yip <john.yip@goanimate.com>
2023-09-19 13:57:15 +0800

    Apply before-login templates

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
 import './globals.css'
-import Navbar from './Navbar'
-
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 export const metadata: Metadata = {
   title: 'Conduit',
@@ -17,9 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
