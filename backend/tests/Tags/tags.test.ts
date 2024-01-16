67aa3147a18f6c20e794991ed35dfd6c3eea047a
John-Yip-HK <yip4work@gmail.com>
2024-01-16 20:35:49 +0800

    Rewrite `/tags` test

--- backend/tests/Tags/tags.test.ts
+++ backend/tests/Tags/tags.test.ts
@@ -10,11 +10,13 @@ const response = {
   send: vi.fn(() => {}),
 } as unknown as Response<TagResponse>;
 
-test('Get tags successfully', () => {
-  const { send: mockedSend } = response;
+describe('getTagsController', () => {
+  test('Get tags successfully', () => {
+    const { send: mockedSend } = response;
+    
+    getTagsController(request, response);
   
-  getTagsController(request, response);
-
-  expect(mockedSend).toHaveBeenCalledTimes(1);
-  expect(mockedSend).toHaveBeenCalledWith({ tags });
-});
+    expect(mockedSend).toHaveBeenCalledTimes(1);
+    expect(mockedSend).toHaveBeenCalledWith({ tags });
+  });
+})
